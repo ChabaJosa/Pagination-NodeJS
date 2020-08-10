fb5dca82dd3648ea20f174a403e64782980bbfaa
ChabaJosa <chabagjg@gmail.com>
2020-08-09 21:55:13 -0400

    Did some cleaning

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -1,15 +1,42 @@
-// const express = require("express");
-// const User = require("../models/user");
-// const app = express();
+const express = require("express");
+const User = require("../models/user");
+const app = express();
 
-// app.get("/users", async (req, res) => {
-//   const users = await User.find({} );
+app.get("/users", paginatedResults(User), (req, res) => {
+  res.json(res.paginatedResults);
+});
 
-//   try {
-//     res.send(users);
-//   } catch (err) {
-//     res.status(500).send(err);
-//   }
-// });
+function paginatedResults(model) {
+  return async (req, res, next) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
 
-// module.exports = app;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const result = {};
+    if (endIndex < model.countDocuments().exec()) {
+      result.next = {
+        page: page + 1,
+        limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      result.previous = {
+        page: page - 1,
+        limit,
+      };
+    }
+
+    try {
+      result.results = await model.find().limit(limit).skip(startIndex).exec();
+      res.paginatedResults = result;
+      next();
+    } catch (e) {
+      res.status(500).json({ message: e.message });
+    }
+  };
+}
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,54 +1,15 @@
 const express = require("express");
 const colors = require("colors");
-const mainRouter = require("./routes/mainRouter");
 
+const mainRouter = require("./routes/mainRouter");
 const connectDB = require("./db");
-const User = require("./models/user");
 
 const app = express();
-app.use(express.json());
 
 connectDB();
-//     .then(() => {
-app.get("/users", paginatedResults(User), (req, res) => {
-  //   console.log(res);
-  res.json(res.paginatedResults);
-});
-// });
-// app.use(mainRouter)
 
-function paginatedResults(model) {
-  return async (req, res, next) => {
-    const page = parseInt(req.query.page);
-    const limit = parseInt(req.query.limit);
+app.use(mainRouter)
 
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
-
-    const result = {};
-    if (endIndex < model.countDocuments().exec()) {
-      result.next = {
-        page: page + 1,
-        limit,
-      };
-    }
-
-    if (startIndex > 0) {
-      result.previous = {
-        page: page - 1,
-        limit,
-      };
-    }
-
-    try {
-      result.results = await model.find().limit(limit).skip(startIndex).exec();
-      res.paginatedResults = result;
-      next();
-    } catch (e) {
-      res.status(500).json({ message: e.message });
-    }
-  };
-}
 
 const PORT = process.env.PORT || 5000;
 
