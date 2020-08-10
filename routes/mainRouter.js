4d447b68b80496696d9ae6bf75f142a8ee76c2f7
ChabaJosa <chabagjg@gmail.com>
2020-08-10 13:40:09 -0400

    Need to work on ordering by id, about to deploy to heroku

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -2,35 +2,37 @@ const express = require("express");
 const User = require("../models/user");
 const app = express();
 
-app.get("/users", paginatedResults(User), (req, res) => {
+app.get("/apps", paginatedResults(User), (req, res) => {
   res.json(res.paginatedResults);
 });
 
 function paginatedResults(model) {
   return async (req, res, next) => {
-    const page = parseInt(req.query.page);
-    const limit = parseInt(req.query.limit);
-
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
+    const by = req.query.by;
+    const startIndex = parseInt(req.query.start) - 1 || 0;
+    const endIndex = parseInt(req.query.end);
+    const maxIndex = parseInt(req.query.max) + 1;
+    const order = req.query.order;
 
     const result = {};
-    if (endIndex < model.countDocuments().exec()) {
-      result.next = {
-        page: page + 1,
-        limit,
-      };
-    }
-
-    if (startIndex > 0) {
-      result.previous = {
-        page: page - 1,
-        limit,
-      };
-    }
 
     try {
-      result.results = await model.find().limit(limit).skip(startIndex).exec();
+      let rawDataArray = await model.find({}, { _id: 0 }).exec();
+      result.results = rawDataArray.slice(
+        startIndex,
+        startIndex + maxIndex - 1 || endIndex || startIndex + 50
+      );
+
+      if (by === "name") {
+        // console.log("Name", by, order);
+        order === "asc" ? result.results.sort() : result.results.reverse();
+      } else if (by === "id") {
+        // console.log("id", by, order);
+        order === "asc"
+          ? result.results.sort((a, b) => a.id - b.id) // Not Working Properly
+          : result.results.sort((a, b) => b.id - a.id); // Not Working Properly
+      }
+
       res.paginatedResults = result;
       next();
     } catch (e) {
