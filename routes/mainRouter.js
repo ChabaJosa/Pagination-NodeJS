c26d6c99758ced55318c193a0feded8064ee9762
ChabaJosa <chabagjg@gmail.com>
2020-08-10 23:00:28 -0400

    Finished all reqs, probably could use some refactoring, added dummy text to read me

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -2,31 +2,51 @@ const express = require("express");
 const User = require("../models/user");
 const app = express();
 
+app.get("/apps", paginatedResults(User), (req, res) => {
+  res.json(res.paginatedResults);
+});
+
 app.get("/apps/:by", paginatedResults(User), (req, res) => {
   res.json(res.paginatedResults);
 });
 
 function paginatedResults(model) {
   return async (req, res, next) => {
-    const by          = req.params.by;
-    const startIndex  = parseInt(req.query.start) - 1 || 0;
-    const endIndex    = parseInt(req.query.end);
-    const maxIndex    = parseInt(req.query.max) + 1;
-    const order       = req.query.order;
-
+    const by = req.params.by;
+    const order = req.query.order;
+    const maxIndex = parseInt(req.query.max);
     const result = {};
 
     try {
-      let helperIndex =
-        startIndex + maxIndex - 1 > endIndex
-          ? endIndex
-          : startIndex + maxIndex - 1;
+      if (by === "id") {
+        const startIndex = parseInt(req.query.start) - 1 || 0;
+        const endIndex = parseInt(req.query.end);
 
-      let rawDataArray = await model.find({}, { _id: 0 }).exec();
-      result.results = rawDataArray.slice(
-        startIndex,
-        helperIndex || endIndex || startIndex + 50
-      );
+        let helperIndex =
+          startIndex + maxIndex - 1 > endIndex
+            ? endIndex
+            : startIndex + maxIndex ;
+
+        let rawDataArray = await model.find({}, { _id: 0 }).exec();
+        result.results = rawDataArray.slice(
+          startIndex,
+          helperIndex || endIndex || startIndex + 50
+        );
+      } else {
+        const startIndex = parseInt(/\d+/.exec(req.query.start)) - 1 || 0;
+        const endIndex = parseInt(/\d+/.exec(req.query.end));
+
+        let helperIndex =
+          startIndex + maxIndex  > endIndex
+            ? endIndex
+            : startIndex + maxIndex;
+
+        let rawDataArray = await model.find({}, { _id: 0 }).exec();
+        result.results = rawDataArray.slice(
+          startIndex,
+          helperIndex || endIndex || startIndex + 50
+        );
+      }
 
       order === "desc" ? result.results.reverse() : result.results.sort();
 
